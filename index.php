aa03b2ca7b99b3e7f49aea7c1d701f032837950d
Kenechukwu Frank <kenisank1@gmail.com>
2021-12-31 14:30:27 +0100

    done with the submit button

--- index.php
+++ index.php
@@ -7,22 +7,27 @@
 
     </head>
     <body>
-
-    
-        <div class="mainSection">
-            <div class="logoContainer">
-<img src="https://www.google.com/logos/doodles/2021/new-years-eve-2021-6753651837109170-6752733080595621-cst.gif">
-           <!-- NAU PROJECT SEARCH -->
-</div>
-<div class="searchContainer">
-
-<form action="search.php" method="GET">
-
-    <input class="searchBox" type="text"  name="term" >
-    <input class="searchButton" type="submit"  value="Search" >
-
-</form>
-</div>
+    <div class="wrapper indexPage">
+        
+            <div class="mainSection">
+                    <div class="logoContainer">
+        <img src="https://www.google.com/logos/doodles/2021/new-years-eve-2021-6753651837109170-6752733080595621-cst.gif">
+                <!-- NAU PROJECT SEARCH -->
         </div>
+        <div class="searchContainer">
+
+        <form action="search.php" method="GET">
+
+            <input class="searchBox" type="text"  name="term" >
+            <input class="searchButton" type="submit"  value="Search" >
+
+        </form>
+        </div>
+                </div>
+
+
+
+
+    </div>
     </body> 
 </html>
