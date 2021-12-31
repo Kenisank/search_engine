4352377ce2db5b2af131b58df160e6813857b089
Kenechukwu Frank <kenisank1@gmail.com>
2021-12-31 14:02:20 +0100

    next\

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+        <title>NAU BSc. Project Search</title>
+   
+        <link rel="stylesheet" type="text/css" href="assets/style.css">
+
+    </head>
     <body>
+
+    
         <div class="mainSection">
             <div class="logoContainer">
 <img src="https://www.google.com/logos/doodles/2021/new-years-eve-2021-6753651837109170-6752733080595621-cst.gif">
-            </div>
+           <!-- NAU PROJECT SEARCH -->
+</div>
 <div class="searchContainer">
 
 <form action="search.php" method="GET">
