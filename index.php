023151c101a5549523b0dac6ade1f82a90c9bc91
Kenechukwu Frank <kenisank1@gmail.com>
2021-12-31 13:33:58 +0100

    Starting

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     <body>
         <div class="mainSection">
             <div class="logoContainer">
-<img src="">
+<img src="https://www.google.com/logos/doodles/2021/new-years-eve-2021-6753651837109170-6752733080595621-cst.gif">
             </div>
+<div class="searchContainer">
 
+<form action="search.php" method="GET">
+
+    <input class="searchBox" type="text"  name="term" >
+    <input class="searchButton" type="submit"  value="Search" >
+
+</form>
+</div>
         </div>
     </body> 
 </html>
