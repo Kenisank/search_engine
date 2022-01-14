61a22be402b5d716f18af42fb67b7e4307d17329
Kenechukwu Frank <kenisank1@gmail.com>
2022-01-14 10:41:46 +0100

    back to work

--- search.php
+++ search.php
@@ -7,9 +7,26 @@
 
     </head>
     <body>
-    <div class="wrapper indexPage">
+    <div class="wrapper">
         
-           
+           <div class="header">
+
+
+           <div class="headerContent">
+           <div class="logoContainer">
+        <a href="index.php"><img src="assets/images/doodleLogo.png"></a>
+                <!-- NAU PROJECT SEARCH -->
+        </div>
+        <div class="searchContainer">
+<form action="search.php" method="GET">
+<div class="searchBarContainer">
+<input class="searchBox" type="text" name="term">
+<button>Search</button>
+</div>
+</form>
+        </div>
+           </div>
+           </div>
 
 
 
