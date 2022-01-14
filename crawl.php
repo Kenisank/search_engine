71b07331381bdd07c479536e90d0a96835106721
Kenechukwu Frank <kenisank1@gmail.com>
2022-01-14 15:53:46 +0100

    stop for now

---
+++ crawl.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function followLinks($url){
+
+    
+
+
+}
+
+
+$startUrl="http://www.reecekenny.com";
+followLinks($startUrl);
+
+
+?>
--- search.php
+++ search.php
@@ -1,3 +1,17 @@
+<?php
+
+if(isset($_GET["term"])){
+    $term=$_GET["term"];
+}
+else{
+    exit("You must enter a search value");
+}
+
+
+
+$type=isset($_GET["type"]) ? $_GET["type"] : "sites";
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,15 +35,23 @@
 <form action="search.php" method="GET">
 <div class="searchBarContainer">
 <input class="searchBox" type="text" name="term">
-<button>Search</button>
+<button class="searchButton"><img src="assets/images/icons/search.png"></button>
 </div>
 </form>
         </div>
            </div>
+
+           <div class="tabsContainer">
+<ul class="tabList">
+
+<li class="<?php echo $type== 'sites' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a></li>
+<li class="<?php echo $type== 'images' ? 'active' : '' ?>"><a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a></li>
+  
+</ul>
            </div>
 
 
-
+           </div>
 
     </div>
     </body> 
